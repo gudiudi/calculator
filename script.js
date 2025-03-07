44a06f695610dc2ade270e085dbd05a6d5df2989
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-07 14:40:09 +0700

    feat: add operand 'a' logic for storing and diplaying the value

--- script.js
+++ script.js
@@ -18,11 +18,29 @@ function operate() {
   
 }
 
+function isNumeric(value) {
+  return !isNaN(value) && !isNaN(parseFloat(value))
+}
+
 (() => {
   const calculatorButtons = document.querySelector('.calculator-buttons');
+  const calculatorDisplayValue = document.querySelector('.calculator-display-value');
+
+  let a = '', 
+      b = '', 
+      operator = '';
 
   calculatorButtons.addEventListener('click', (e) => {
     const target = e.target;
-    console.log(target.textContent);
+    const value = target.textContent;
+    
+    if (
+      isNumeric(value) || 
+      (value === '.' && !calculatorDisplayValue.textContent.includes('.')) ||
+      (value === '-' && a[0] !== '-')
+    ) {
+      a += value;
+      calculatorDisplayValue.textContent = a;
+    }
   });
 })();
