cbdf2cd4e28e4b724e5caffc19bb53bf9654a54d
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-07 16:10:50 +0700

    feat: changed if else logic in button listener to parse expression function and add basic calulation  functionality

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
-function add() {
-
+function add(a, b) {
+  return a + b;
 }
 
-function subtract() {
-
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide() {
-
+function divide(a, b) {
+  return a / b;
 }
 
-function operate() {
-  
+function modulus(a, b) {
+  return a % b;
 }
 
-function isNumeric(value) {
-  return !isNaN(value) && !isNaN(parseFloat(value))
+function calculateExpression(a, b, operator) {
+  const operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+    '%': modulus
+  };
+
+  return operations[operator] ? operations[operator](a, b) : 0;
+}
+
+function parseExpression(exp) {
+  const expressionArr = exp.split('');
+
+  const operators =  ['+', '-', '*', '/', '%'];
+  const isOperator = (x) => operators.includes(x);
+  const getOperator = (arr) => arr.filter(x => isOperator(x));
+  const getOperatorIndex = (arr, operator) => arr.findIndex(x => x === operator);
+
+  const operator = getOperator(expressionArr);
+  const operatorIndex = getOperatorIndex(expressionArr, operator[0]);
+
+  const operandA = expressionArr.slice(0, operatorIndex);
+  const operandB = expressionArr.slice(operatorIndex + 1, expressionArr.length);
+
+  return [Number(operandA.join('')), Number(operandB.join('')), operator[0]];
 }
 
 (() => {
   const calculatorButtons = document.querySelector('.calculator-buttons');
   const calculatorDisplayValue = document.querySelector('.calculator-display-value');
 
-  let a = '', 
-      b = '', 
-      operator = '';
-
   calculatorButtons.addEventListener('click', (e) => {
     const target = e.target;
     const value = target.textContent;
-    
-    if (
-      isNumeric(value) || 
-      (value === '.' && !calculatorDisplayValue.textContent.includes('.')) ||
-      (value === '-' && a[0] !== '-' && a.length === 0)
-    ) {
-      a += value;
-      calculatorDisplayValue.textContent = a;
+
+    if (value === '=') {
+      const [operandA, operandB, operator] = parseExpression(calculatorDisplayValue.textContent);
+      const result = calculateExpression(operandA, operandB, operator);
+      console.log(result);
+      calculatorDisplayValue.textContent = result;
+    } else {
+      calculatorDisplayValue.textContent += value;
     }
   });
 })();
