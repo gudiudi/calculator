86451459efb786e3c48d0fa835289e667df17c12
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-07 13:39:03 +0700

    feat: add listen and capture the text of the buttons when clicked

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ function divide() {
 function operate() {
   
 }
+
+(() => {
+  const calculatorButtons = document.querySelector('.calculator-buttons');
+
+  calculatorButtons.addEventListener('click', (e) => {
+    const target = e.target;
+    console.log(target.textContent);
+  });
+})();
